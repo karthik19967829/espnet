5f9a6f4b74c6396980002a38231cb8ed25ff578f
Karthik Ganesan <Karthikganesan17@gmail.com>
2022-10-14 14:55:50 -0400

    Update add_adapters.py

--- espnet2/asr/frontend/adapter_utils/add_adapters.py
+++ espnet2/asr/frontend/adapter_utils/add_adapters.py
@@ -7,7 +7,7 @@ from espnet2.asr.frontend.adapter_utils.adapters.adapter_transformer import (
 
 def add_adapters(s3prl_upstream_model, adapter_down_dim, adapt_layers=None):
     """
-    add adapters to wav2vec2 model.
+    add adapters to S3PRL upstream model.
     * adapter_down_dim - down-projection dimension of adapter.
     * adapt_layers - list of indices of layers to insert adapters. If `None`, adapters are inserted to every layer. (default=`None`).
     """
