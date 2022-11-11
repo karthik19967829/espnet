1abd876bb462fe6253cc49107316edc2e04d2984
Karthik Ganesan <Karthikganesan17@gmail.com>
2022-11-11 10:09:29 -0500

    Update adapter.py

--- espnet2/asr/frontend/adapter_utils/adapters/adapter.py
+++ espnet2/asr/frontend/adapter_utils/adapters/adapter.py
@@ -2,15 +2,6 @@ import torch
 import torch.nn as nn
 
 
-try:
-    from fairseq import utils
-    from fairseq.modules import LayerNorm
-
-    is_fairseq_available = True
-except ImportError:
-    is_fairseq_available = False
-
-
 class Adapter(nn.Module):
     def __init__(
         self,
@@ -26,22 +17,16 @@ class Adapter(nn.Module):
         * activation_fn - activation type (default="gelu").
         """
         super().__init__()
-        if not is_fairseq_available:
-            raise ImportError(
-                "`fairseq` is not available. Please install it via `pip install"
-                " fairseq` or `cd /path/to/espnet/tools && . ./activate_python.sh"
-                " && ./installers/install_fairseq.sh`."
-            )
 
         self.down_projection = nn.Linear(orig_dim, down_dim)
         self.up_projection = nn.Linear(down_dim, orig_dim)
         nn.init.xavier_uniform_(self.down_projection.weight)
         nn.init.xavier_uniform_(self.up_projection.weight)
-        self.activation = utils.get_activation_fn(activation_fn)
+        self.activation = nn.GELU()
 
         self.layer_norm_opt = layer_norm
         if layer_norm is not None:
-            self.layer_norm = LayerNorm(orig_dim)
+            self.layer_norm = nn.LayerNorm(orig_dim)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         if self.layer_norm_opt == "first":
